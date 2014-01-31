75f0b58c6bf7dd81e553cdcbf9c30931067d9cc1
Joseph Pawletko <jgpawletko@gmail.com>
2014-01-31 15:05:26 -0500

    replaced secret_key_base literal with ENV var

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,5 +10,5 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 
-# TODO:store in environment variable and generate new secret_key_base.
-VstructApi::Application.config.secret_key_base = '5ac52e5a9369817ebc4c69518e07fbb80e1bc7534143273ac2ad6ebd7525195f0725d956d980b5e30640e9577c1d509a9992b720acb181ab2cde1956d1800fcc'
+VstructApi::Application.config.secret_key_base = ENV['SECRET_TOKEN']
+
